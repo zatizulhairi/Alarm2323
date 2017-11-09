ebc42a9de9982cae14881c2153840893654e3dc8
zatizulhairi <ztizlhri@gmail.com>
2017-11-09 14:52:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/pc/notifications/AlarmSoundService.java
@@ -0,0 +1,37 @@
+package com.example.pc.notifications;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class AlarmSoundService extends Service {
+    private MediaPlayer mediaPlayer;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //Start media player
+        mediaPlayer = MediaPlayer.create(this, R.raw.alarm);
+        mediaPlayer.start();
+        mediaPlayer.setLooping(false);//set l1ooping true to run it infinitely
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //On destory stop and release the media player
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.reset();
+            mediaPlayer.release();
+        }
+    }
+}
